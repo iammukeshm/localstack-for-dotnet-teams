35a183a8c9579daa81f7d33e0dfc94e1bbd15816
Mukesh Murugan <iammukeshm@gmail.com>
2025-12-26 17:42:03 +0530

    Use dummy AWS credentials and update client config for LocalStack
    Refactored AWS client initialization to use explicit dummy
    credentials ("test"/"test") suitable for LocalStack. Region is now
    handled via RegionEndpoint objects, and AuthenticationRegion is set
    instead of RegionEndpoint. Removed deprecated UseHttp property from
    client configs. All AWS clients now use the updated configuration.

--- LocalStackDemo.Tests/LocalStackFixture.cs
+++ LocalStackDemo.Tests/LocalStackFixture.cs
@@ -1,5 +1,6 @@
 using Amazon;
 using Amazon.DynamoDBv2;
+using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.SQS;
 
@@ -18,36 +19,37 @@ public class LocalStackFixture : IDisposable
     public LocalStackFixture()
     {
         var serviceUrl = Environment.GetEnvironmentVariable("AWS__ServiceUrl") ?? "http://localhost:4566";
-        var region = Environment.GetEnvironmentVariable("AWS__Region") ?? "us-east-1";
+        var region = RegionEndpoint.GetBySystemName(
+            Environment.GetEnvironmentVariable("AWS__Region") ?? "us-east-1");
 
         BucketName = Environment.GetEnvironmentVariable("Resources__BucketName") ?? "orders-receipts";
         TableName = Environment.GetEnvironmentVariable("Resources__TableName") ?? "Orders";
         QueueName = Environment.GetEnvironmentVariable("Resources__QueueName") ?? "orders-events";
 
+        // Use dummy credentials for LocalStack (it doesn't validate them)
+        var credentials = new BasicAWSCredentials("test", "test");
+
         var s3Config = new AmazonS3Config
         {
             ServiceURL = serviceUrl,
             ForcePathStyle = true,
-            UseHttp = true,
-            RegionEndpoint = RegionEndpoint.GetBySystemName(region)
+            AuthenticationRegion = region.SystemName
         };
-        S3Client = new AmazonS3Client(s3Config);
+        S3Client = new AmazonS3Client(credentials, s3Config);
 
         var dynamoConfig = new AmazonDynamoDBConfig
         {
             ServiceURL = serviceUrl,
-            UseHttp = true,
-            RegionEndpoint = RegionEndpoint.GetBySystemName(region)
+            AuthenticationRegion = region.SystemName
         };
-        DynamoDbClient = new AmazonDynamoDBClient(dynamoConfig);
+        DynamoDbClient = new AmazonDynamoDBClient(credentials, dynamoConfig);
 
         var sqsConfig = new AmazonSQSConfig
         {
             ServiceURL = serviceUrl,
-            UseHttp = true,
-            RegionEndpoint = RegionEndpoint.GetBySystemName(region)
+            AuthenticationRegion = region.SystemName
         };
-        SqsClient = new AmazonSQSClient(sqsConfig);
+        SqsClient = new AmazonSQSClient(credentials, sqsConfig);
     }
 
     public void Dispose()
